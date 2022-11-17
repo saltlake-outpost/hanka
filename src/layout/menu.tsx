81003a00ac2dc2238ed6e54a46be95772f6cc92b
Chiemerie <alexemerie7@gmail.com>
2022-11-17 11:00:37 +0200

    Added menu overlay

--- src/layout/menu.tsx
+++ src/layout/menu.tsx
@@ -1,74 +1,79 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {env} from "../environment";
 import {Link} from "react-router-dom";
 
 export const Menu = () => {
-  return (
-      <section data-bs-version="5.1" className="menu1 cid-tc91E4QaPr" id="menu1-46">
-          <nav className="navbar navbar-dropdown collapsed">
-              <div className="container-fluid">
-                  <div className="navbar-brand">
-                <span className="navbar-logo">
-                    <Link to="/">
-                        <img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" alt=""
-                             style={{ height: "5rem" }} loading="lazy" className="lazyload"
-                             data-src="/images/logo.png"/>
-                    </Link>
-                </span>
-                      <span className="navbar-caption-wrap">
-                          <Link className="navbar-caption text-black text-primary display-5" to="/src/pages">
-                              {env.appName}
-                          </Link>
-                      </span>
-                  </div>
-                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse"
-                          data-target="#navbarSupportedContent" data-bs-target="#navbarSupportedContent"
-                          aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                      <div className="hamburger">
-                          <span></span>
-                          <span></span>
-                          <span></span>
-                          <span></span>
-                      </div>
-                  </button>
-                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                      <ul className="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
-                          <li className="nav-item">
-                              <Link className="nav-link link text-black text-primary display-4" to="about-us.html"
-                                 aria-expanded="false">
-                                  ABOUT US<br/>
-                                  <div className="line-animation"></div>
-                              </Link>
-                          </li>
-                          <li className="nav-item">
-                              <Link className="nav-link link text-black text-primary display-4" to="services.html"
-                                 aria-expanded="false">
-                                  SERVICES<br/>
-                                  <div className="line-animation"></div>
-                              </Link>
-                          </li>
-                          <li className="nav-item">
-                              <Link className="nav-link link text-black text-primary display-4" to="projects.html">
-                                  PROJECTS<br/>
-                                  <div className="line-animation"></div>
-                              </Link>
-                          </li>
-                          <li className="nav-item">
-                              <Link className="nav-link link text-black text-primary display-4" to="careers.html">
-                                  CAREERS<br/>
-                                  <div className="line-animation"></div>
-                              </Link>
-                          </li>
-                          <li className="nav-item">
-                              <Link className="nav-link link text-black text-primary display-4" to="contact-us.html">
-                                  CONTACT US<br/>
-                                  <div className="line-animation"></div>
-                              </Link>
-                          </li>
-                      </ul>
-                  </div>
-              </div>
-          </nav>
-      </section>
-  );
+    const [displayMenu, setDisplayMenu] = useState<'none' | 'block'>('none');
+    const closeNav = () => {
+        setDisplayMenu('none');
+    }
+
+    return (
+        <section data-bs-version="5.1" className="menu1 cid-tc91E4QaPr" id="menu1-46">
+            <nav className="navbar navbar-dropdown navbar-light collapsed">
+                <div className="container-fluid">
+                    <div className="navbar-brand">
+                        <span className="navbar-logo">
+                            <Link to="/">
+                                <img src="/images/logo.png" alt="" style={{ height: "5rem" }} loading="lazy"
+                                     className="lazyload" data-src="/images/logo.png"/>
+                            </Link>
+                        </span>
+                        {/*<span className="navbar-caption-wrap">
+                            <Link className="navbar-caption text-black text-primary display-5"
+                                  to="//src/pages">{env.appName}</Link>
+                        </span>*/}
+                    </div>
+                    <button className="navbar-toggler" onClick={() => setDisplayMenu('block')}>
+                        <div className="hamburger">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </button>
+                    <div style={{ display: displayMenu }} className="overlay">
+                        <Link to="#" className="closebtn" onClick={closeNav}>&times;</Link>
+                        <ul className="navbar-nav nav-dropdown navbar-light nav-right overlay-content" data-app-modern-menu="true">
+                            <li className="nav-item">
+                                <Link className="nav-link link text-black text-primary display-4"
+                                      to="/about-us" aria-expanded="false" onClick={closeNav}>
+                                    ABOUT US<br/>
+                                    <div className="line-animation"></div>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link link text-black text-primary display-4" to="/services"
+                                      aria-expanded="false" onClick={closeNav}>
+                                    SERVICES<br/>
+                                    <div className="line-animation"></div>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link link text-black text-primary display-4"
+                                      to="/projects" onClick={closeNav}>
+                                    PROJECTS<br/>
+                                    <div className="line-animation"></div>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link link text-black text-primary display-4"
+                                      to="/careers" onClick={closeNav}>
+                                    CAREERS<br/>
+                                    <div className="line-animation"></div>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link link text-black text-primary display-4"
+                                      to="/contact-us" onClick={closeNav}>
+                                    CONTACT US<br/>
+                                    <div className="line-animation"></div>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </section>
+    );
 }
