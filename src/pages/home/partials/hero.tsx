9741a9be4680f316aa101cd95a11b5a2c189bf46
Chiemerie <alexemerie7@gmail.com>
2022-11-17 20:53:40 +0200

    centered background video

--- src/pages/home/partials/hero.tsx
+++ src/pages/home/partials/hero.tsx
@@ -3,10 +3,11 @@ import React from "react";
 export const Hero = () => {
     return (
         <section data-bs-version="5.1" className="header2 cid-tl9ZQYcfLu mbr-fullscreen" id="header2-4f">
-            <video playsInline={true} autoPlay={true} loop={true} muted={true}
-                   style={{ minHeight: "100vh", minWidth: "100vw", position: "absolute", top: 0 }}>
-                <source src={"https://fprs.help/bg-video.mp4"} type={"video/mp4"}/>
-            </video>
+            <div className={"video-container"}>
+                <video playsInline={true} autoPlay={true} loop={true} muted={true}>
+                    <source src={"https://fprs.help/bg-video.mp4"} type={"video/mp4"}/>
+                </video>
+            </div>
             <div className="mbr-overlay" style={{ opacity: "0.4", backgroundColor: "rgb(19, 40, 125)" }}/>
             <div className="main-container">
                 <div className="row">
